20ef5ac5901031ace2cd7cf4f8ea357bfa10384a
eric <eacraft13@gmail.com>
2017-07-12 16:46:05 -0500

    Simplify install
    Don't include unneccesary installs and libs

--- tao/ubuntu/trusty64/README.md
+++ tao/ubuntu/trusty64/README.md
@@ -4,13 +4,13 @@ Creates a prepacked vagrant base box to use for Tao development.
 - PHP 5.6
 - Apache 2.4
 - MySQL 5.5
-- NVM & RVM
+- NVM
 
 
 #### Instructions to create a vagrant box
 
 - `$ vagrant init ubuntu/trusty64`
 - `$ vagrant up`
-- `$ vagrant ssh -c 'source /vagrant/lib/init.sh'`
+- `$ vagrant ssh -c 'source /vagrant/init.sh'`
 - `$ vagrant package --output tao-ubuntu-trusty64.box`
 - `$ vagrant box add tao/ubuntu/trusty64 tao-ubuntu-trusty64.box`
---
+++ tao/ubuntu/trusty64/init.sh
@@ -0,0 +1,63 @@
+# Updating & upgrading
+
+sudo apt-get -qq update
+sudo apt-get -y -qq upgrade
+
+
+# Bash
+
+sudo cp /vagrant/lib/bashrc $HOME/.bashrc
+sudo cp /vagrant/lib/bash_aliases $HOME/.bash_aliases
+source $HOME/.bashrc
+
+
+# Curl
+
+sudo apt-get -y -qq install curl
+
+
+# Apache
+
+sudo apt-get -y -qq install apache2
+
+sudo mkdir /var/www/taoplatform
+sudo gpasswd -a "$USER" www-data
+
+sudo cp /vagrant/lib/taoplatform.conf /etc/apache2/sites-available/taoplatform.conf
+sudo a2ensite taoplatform
+sudo service apache2 reload
+sudo a2enmod rewrite
+sudo service apache2 restart
+
+
+# MySQL
+
+export MYSQL_PASS=root
+export DEBIAN_FRONTEND=noninteractive
+sudo debconf-set-selections <<EOT
+mysql-server mysql-server/root_password password $MYSQL_PASS
+EOT
+sudo debconf-set-selections <<EOT
+mysql-server mysql-server/root_password_again password $MYSQL_PASS
+EOT
+sudo apt-get -y -qq install mysql-server
+
+
+# PHP
+
+sudo apt-get -y -qq install python-software-properties
+sudo add-apt-repository -y ppa:ondrej/php
+sudo apt-get -qq update
+sudo apt-get -y -qq install php5.6 php5.6-gd php5.6-mysql php5.6-tidy php5.6-curl php5.6-mbstring php5.6-zip php5.6-xml php5.6-mcrypt php-xml-parser
+
+sudo a2dismod php7.0 ; sudo a2enmod php5.6 ; sudo service apache2 restart # Set php5.6 for Apache
+sudo update-alternatives --set php /usr/bin/php5.6 # Set php5.6 for CLI
+
+
+# Node
+
+sudo apt-get -y -qq install build-essential libssl-dev
+curl -o- https://raw.githubusercontent.com/creationix/nvm/v0.33.0/install.sh | bash
+export NVM_DIR="$HOME/.nvm"
+[ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh" # This loads nvm
+nvm install node
